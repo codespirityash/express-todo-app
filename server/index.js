7b8ee7e80baddee606504e2f13c22840b22234a6
codespirityash <codespirityash@gmail.com>
2025-02-21 22:42:41 +0530

    add get,post endpoints

--- server/index.js
+++ server/index.js
@@ -1,14 +1,48 @@
 import express from "express";
-// const express = require("express");
-// const bodyparser = require("body-parser");
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// const app = express();
-// app.use(bodyparser.json());
-// console.log("Server is starting...");
 const app = express();
+const port = 3030;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dbPath = path.join(__dirname, "database.json");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("HELLO WORLD");
+  res.send(`
+    <html>
+      <head>
+        <title>Express Server</title>
+      </head>
+      <body>
+        <h1>HELLO WORLD</h1>
+      </body>
+    </html>
+  `);
 });
 
-app.listen(3030, () => console.log("Example app listening on port 3000!"));
+app.get("/all_items", (req, res) => {
+  const data = fs.readFileSync(dbPath, "utf-8");
+  const db = JSON.parse(data);
+  res.json(db.users);
+});
+
+app.post("/item", (req, res) => {
+  const newUser = req.body;
+  if (!newUser.name || newUser.age === undefined) {
+    return res.status(400).json({ error: "Please provide both name and age." });
+  }
+  const data = fs.readFileSync(dbPath, "utf-8");
+  const db = JSON.parse(data);
+  db.users.push(newUser);
+  fs.writeFileSync(dbPath, JSON.stringify(db, null, 2), "utf-8");
+  res.status(201).json({ message: "User added successfully.", user: newUser });
+});
+
+app.listen(port, () =>
+  console.log(`Server is running on http://localhost:${port}`)
+);
