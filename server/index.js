72885c2f10d0681832ddfa65127813b0fb633a5f
yashweblife <yashweblife20@gmail.com>
2025-04-04 12:27:39 -0500

    initial working setup

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ import { createNewUserDB, initializeDBIfNotExists, verifyIfUserExists } from "./
 
 const app = express();
 app.use(express.json())
-console.log("Hello World")
 initializeDBIfNotExists();
 app.use(cors({
     allowedHeaders: "*",
@@ -17,6 +16,11 @@ app.get("/",(req,res)=>{
 })
 
 app.post("/login",(req,res)=>{
+    /**
+     * Body structure
+     * email string
+     * password string
+     */
     const body = req.body
     const uid = verifyIfUserExists(body.email, body.password);
     if(uid){
@@ -32,6 +36,12 @@ app.post("/login",(req,res)=>{
 })
 
 app.post("/signup",async (req,res)=>{
+    /**
+     * Body structure
+     * email string
+     * name string
+     * password string
+     */
     const body = req.body
     const uid = await createNewUserDB(body.email, body.name, body.password);
     console.log(uid)
@@ -46,7 +56,70 @@ app.post("/signup",async (req,res)=>{
         })
     }
 })
+app.post("/create-new-todo", (req,res)=>{
+    /**
+     * Body structure
+     * uid string
+     * title string
+     */
+    const body = req.body;
+    const id = createNewTodoForUser(body.uid, body.title);
+    if(id){
+        res.send({
+            message:"Todo Created Successfully",
+            id
+        })
+    } else {
+        res.send({
+            message:"Todo Creation Failed"
+        })
+    }
 
+})
+app.post("toggle-todo",(req,res)=>{
+    const {uid, todoId, state} = req.body;
+    const id = toggleTodoCompleted(uid, todoId, state);
+    if(id){
+        res.send({
+            message:"Todo Toggled Successfully",
+            id
+        })
+    } else {
+        res.send({
+            message:"Todo Toggle Failed"
+        })
+    }
+})
+app.post("delete-todo",(req,res)=>{
+    const {uid, todoId} = req.body;
+    const id = deleteTodo(uid, todoId);
+    if(id){
+        res.send({
+            message:"Todo Deleted Successfully",
+            id
+        })
+    } else {
+        res.send({
+            message:"Todo Deletion Failed"
+        })
+    }
+})
+app.post("get-todos",(req,res)=>{
+    const {uid} = req.body;
+    const todos = getUserTodoDB(uid);
+    if(todos){
+        res.send({
+            message:"Todos Fetched Successfully",
+            todos
+        })
+    } else {
+        res.send({
+            message:"Todo Fetch Failed"
+        })
+    }
+})
 app.listen(3030, () => {
     console.log("Server running on port 3030");
+    console.log("API ENDPOINTS:\n")
+    console.log("1. POST:/login\n2. POST:/signup\n3. POST:/create-new-todo\n4. POST:/toggle-todo\n5. POST:/delete-todo\n6. POST/get-todos")
 })
