d121d839fc2ee782fe9e839b235a9ad855923740
yashweblife <yashweblife20@gmail.com>
2025-04-04 12:27:25 -0500

    register manager

---
+++ server/lib/registrer.js
@@ -0,0 +1,40 @@
+import cors from "cors";
+import express from "express";
+export default class Register {
+    routes = [];
+    app = express();
+    constructor() {
+        this.app.use(cors({
+            allowedHeaders: "*",
+        }))
+        this.app.use(express.json())
+    }
+    /**
+     * @param {"get" | "post"} method 
+     * @param { string } route 
+     * @param {(req:express.Request, res:express.Response)=>void} callback 
+     */
+    add(method, route, callback) {
+        if (method === "get") {
+            this.app.get(route, callback)
+        } else if (method === "post") {
+            this.app.post(route, callback)
+        }
+        this.routes.push({ method: method.toUpperCase, route: route })
+    }
+    logRoutes() {
+        console.log("\nAPI ENDPOINTS:\n")
+        this.routes.forEach(route => {
+            console.log(`${route.method} ${route.route}`)
+        });
+    }
+    /**
+     * @param {number} port 
+     */
+    start(port = 3030) {
+        this.app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+            this.logRoutes();
+        })
+    }
+}
